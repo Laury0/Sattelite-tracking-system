260e2a7b4947674fe493bcc7d7186cf6da511647
Laury0 <laurynasdavi@gmail.com>
2026-04-25 14:21:40 +0300

    fixed legend, and made it bold

--- SateliteTrackingSystem/Map/Map.py
+++ SateliteTrackingSystem/Map/Map.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import os
+import matplotlib.patheffects as path_effects
 from Core.Space_Objects import Satellite, GroundStation
 
 class TwoD_Map:
@@ -43,6 +44,23 @@ class TwoD_Map:
         alt, az, distance = topocentric.altaz()
         return alt.degrees > 0
 
+    def legend(self, satellites):
+        start_y = 20
+
+        for i, sat in enumerate(satellites):
+            if isinstance(sat, GroundStation):
+                lat, lon = sat.lat, sat.lon
+                alt = 0
+            else:
+                geo = sat.get_position()
+                lat = geo.latitude.degrees
+                lon = geo.longitude.degrees
+                alt = geo.elevation.km
+
+            text = f"{sat.name[:10]} | {lat:.1f}, {lon:.1f} | {alt:.0f} km"
+            t = self.map_axes.text(10,start_y + i * 15,text,fontsize=8,color=sat.color,fontweight='bold')
+            t.set_path_effects([path_effects.Stroke(linewidth=2, foreground='black'),path_effects.Normal()])
+
     def update(self, manager):
         self.map_axes.clear()
         self.setup()
@@ -52,7 +70,8 @@ class TwoD_Map:
                 lat, lon = obj.lat, obj.lon
                 self.draw_point(lat, lon, obj.color)
                 x, y = self.convert(lat, lon)
-                self.map_axes.text(x + 5, y + 5, obj.name, fontsize=8)
+                text = self.map_axes.text(x, y, obj.name, fontsize=6, color=obj.color)
+                text.set_path_effects([path_effects.Stroke(linewidth=2, foreground='black'),path_effects.Normal()])
 
             elif isinstance(obj, Satellite):
                 geo = obj.get_position()
@@ -73,4 +92,7 @@ class TwoD_Map:
                 if self.is_visible(sat, ground):
                     geo = sat.get_position()
                     sx, sy = self.convert(geo.latitude.degrees, geo.longitude.degrees)
-                    self.map_axes.plot([gx, sx], [gy, sy], color="red")
+                    text = self.map.map_axes.text(x + 5, y + 5, obj.name, fontsize=8, color=obj.color)
+                    text.set_path_effects([path_effects.Stroke(linewidth=2, foreground='black'),path_effects.Normal()])
+
+        self.legend(manager.active)
